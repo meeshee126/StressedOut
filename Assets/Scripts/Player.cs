7e2a97d216d7527a86a907b5b3365cfa10b3a0ed
Michael Schmidt <schmidtmichael89@hotmail.de>
2019-10-09 12:37:13 +0200

    Added Player and not finished timer

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float movementSpeed;
+
+    Rigidbody2D rb;
+ 
+    void Start()
+    {
+        rb.GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        Vector2 move = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+        rb.velocity = move * movementSpeed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    float totalTime;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        totalTime -= Time.deltaTime;
+        float seconds = totalTime % 60;
+        Debug.Log(seconds);
+    }
+}
