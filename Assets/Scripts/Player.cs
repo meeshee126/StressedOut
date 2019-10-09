411bb3df3f445c5582fbd930e38b0e70f15a27cf
XMI ~ [Dimi] <ixbayOriginal@gmail.com>
2019-10-09 16:20:33 +0200

    player update wip

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,21 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
 
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    float movementSpeed;
+    private float movementSpeed;
 
-    Rigidbody2D rb;
- 
+    private Rigidbody2D characterRB;
+    private Animator characterAnimator;
+    private CapsuleCollider2D characterCollider;
+
+    private void Awake()
+    {
+        characterRB = GetComponent<Rigidbody2D>();
+        characterAnimator = GetComponent<Animator>();
+        characterCollider = GetComponent<CapsuleCollider2D>();
+    }
+
     void Start()
     {
-        rb.GetComponent<Rigidbody2D>();
+        characterRB.GetComponent<Rigidbody2D>();
     }
     void Update()
     {
-        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        rb.velocity = move * movementSpeed * Time.deltaTime;
+        ApplyInput();
+    }
+
+    /// <summary>
+    /// Translates  [User Input]
+    /// into        [Player Movement].
+    /// </summary>
+    void ApplyInput()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        float moveVertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+
+        transform.Translate(new Vector2(moveHorizontal, moveVertical));
+    }
+
+    /// <summary>
+    /// Updates the [Player's Animation]
+    /// based on    [Player's Movement].
+    /// </summary>
+    /// <param name="moveX"></param>
+    /// <param name="moveY"></param>
+    void AnimationUpdate(float moveX, float moveY)
+    {
+        //Changes Animation Based on direction facing.
+        characterAnimator.SetFloat("FaceX", moveX);
+        characterAnimator.SetFloat("FaceY", moveY);
+
+        if (moveX != 0 || moveY != 0)
+        {
+            characterAnimator.SetBool("isWalking", true);
+            if (moveX > 0) characterAnimator.SetFloat("LastMoveX", 1f);
+            else if (moveX < 0) characterAnimator.SetFloat("LastMoveX", -1f);
+            else characterAnimator.SetFloat("LastMoveX", 0f);
+
+            if (moveY > 0) characterAnimator.SetFloat("LastMoveY", 1f);
+            else if (moveY < 0) characterAnimator.SetFloat("LastMoveY", -1f);
+            else characterAnimator.SetFloat("LastMoveY", 0f);
+        }
+        else
+        {
+            characterAnimator.SetBool("isWalking", false);
+        }
     }
 }
