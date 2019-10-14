6edfe8fba9c2b211c959fc9f315305a0d9af9796
Henrik Drache <thedragonhill@googlemail.com>
2019-10-14 10:42:32 +0200

    new Boundaries/change Area
    Musste ein paar Sachen ändern(lief leider nicht ganz wie geplant)

---
+++ Assets/Scripts/AreaChange.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaChange : MonoBehaviour
+{
+    public Camera MainCamera;
+
+    void OnTriggerEnter2D(Collider2D enter)
+    {
+        if (enter.gameObject.tag == "EnterRight")
+        {
+            MainCamera.transform.position = MainCamera.transform.position + new Vector3(25, 0, 0);
+            transform.position = transform.position + new Vector3(5,0,0);
+        }
+        if (enter.gameObject.tag == "EnterLeft")
+        {
+            MainCamera.transform.position = MainCamera.transform.position + new Vector3(-25, 0, 0);
+            transform.position = transform.position + new Vector3(-5, 0, 0);
+        }
+        if (enter.gameObject.tag == "EnterUp")
+        {
+            MainCamera.transform.position = MainCamera.transform.position + new Vector3(0, 15, 0);
+            transform.position = transform.position + new Vector3(0, 5, 0);
+        }
+        if (enter.gameObject.tag == "EnterDown")
+        {
+            MainCamera.transform.position = MainCamera.transform.position + new Vector3(0, -15, 0);
+            transform.position = transform.position + new Vector3(0, -5, 0);
+        }
+    }
+}
