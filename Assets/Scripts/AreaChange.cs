d125e29f80f4c35985d554427a4c1cfb50920502
Michael Schmidt <schmidtmichael89@hotmail.de>
2019-10-16 17:15:45 +0200

    Added Sun and ColorLerp

--- Assets/Scripts/AreaChange.cs
+++ Assets/Scripts/AreaChange.cs
@@ -6,9 +6,11 @@ public class AreaChange : MonoBehaviour
 {
     public Camera mainCamera;
     Vector3 newPosition;
+    TimeBehaviour timeBehaviour;
 
     private void Start()
     {
+        timeBehaviour = GameObject.Find("GameManager").GetComponent<TimeBehaviour>();
         newPosition = mainCamera.transform.position;
     }
 
@@ -18,21 +20,25 @@ public class AreaChange : MonoBehaviour
         {
             newPosition = mainCamera.transform.position + new Vector3(25, 0, 0);
             transform.position = transform.position + new Vector3(5,0,0);
+            timeBehaviour.timeCost = TimeBehaviour.TimeCost.highCost;  
         }
         if (enter.gameObject.tag == "EnterLeft")
         {
             newPosition = mainCamera.transform.position + new Vector3(-25, 0, 0);
             transform.position = transform.position + new Vector3(-5, 0, 0);
+            timeBehaviour.timeCost = TimeBehaviour.TimeCost.highCost;
         }
         if (enter.gameObject.tag == "EnterUp")
         {
             newPosition = mainCamera.transform.position + new Vector3(0, 15, 0);
             transform.position = transform.position + new Vector3(0, 5.5f, 0);
+            timeBehaviour.timeCost = TimeBehaviour.TimeCost.highCost;
         }
         if (enter.gameObject.tag == "EnterDown")
         {
             newPosition = mainCamera.transform.position + new Vector3(0, -15, 0);
             transform.position = transform.position + new Vector3(0, -5.5f, 0);
+            timeBehaviour.timeCost = TimeBehaviour.TimeCost.highCost;
         }
     }
 
--- Assets/Scripts/Time/TimeSlider.cs
+++ Assets/Scripts/Time/TimeSlider.cs
@@ -9,23 +9,51 @@ public class TimeSlider : MonoBehaviour
 {
     Timer timer;
     Slider slider;
+    Image image;
+
+    [Header("Sun Color")]
+    [SerializeField]
+    Color red;
+    [SerializeField]
+    Color yellow;
+
 
     bool sliderSeted = false;
 
     void Start()
     {
         timer = GameObject.Find("GameManager").GetComponent<Timer>();
+        image = GameObject.Find("Sun").GetComponent<Image>();
+
         slider = GetComponent<Slider>();
     }
 
     void Update()
     {
-        if(!sliderSeted)
+        SetSlider();
+        PanicMode();
+    }
+
+    void SetSlider()
+    {
+        if (!sliderSeted)
         {
             slider.maxValue = timer.TotalTimeInSeconds();
             sliderSeted = true;
         }
 
         slider.value = timer.TotalTimeInSeconds();
+
+        image.color = Color.Lerp(red, yellow, slider.normalizedValue >
+                     (timer.panicTimer / 100) ? slider.normalizedValue : (timer.panicTimer / 100));
+    }
+
+    void PanicMode()
+    {
+        if(slider.normalizedValue <= (timer.panicTimer / 100))
+        {
+            timer.currentDayTime = Timer.DayTime.Panic;
+        }
     }
 }
+
