480ec6e75ce44f65ed694ac4700b15ef5e9676c6
meeshee126 <47327880+meeshee126@users.noreply.github.com>
2019-10-22 21:04:56 +0200

    Changed some things

---
+++ Assets/Scripts/EntityBehaviour/States/IWarned.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IWarned : IState
+{
+  
+    public bool Condition()
+    {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void Execute()
+    {
+        Debug.Log("Warned");
+    }
+}
