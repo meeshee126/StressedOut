0b71f8d73b2aea7a14a7bff43ea48e1ce571b691
Henrik Drache <thedragonhill@googlemail.com>
2019-10-23 12:19:30 +0200

    Add Builds

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    float speed = 20;
+
+    private Transform enemy;
+    private Vector2 target;
+
+    private void Start()
+    {
+        enemy = GameObject.FindGameObjectWithTag("Enemy").transform;
+
+        target = new Vector2(enemy.position.x, enemy.position.y);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        RotateTowards(enemy.position);
+
+        if (transform.position.x == target.x && transform.position.y == target.y)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void RotateTowards(Vector2 target)
+    {
+        Vector2 direction = target - (Vector2)transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+    }
+}
