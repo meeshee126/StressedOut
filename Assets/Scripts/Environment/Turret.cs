da807b5fe6ca62b31bfc7bf38e7ef90afa7feb06
TheDragonHill <46721121+TheDragonHill@users.noreply.github.com>
2019-11-26 20:17:48 +0100

    Last Beta Fix

--- Assets/Scripts/Environment/Turret.cs
+++ Assets/Scripts/Environment/Turret.cs
@@ -10,53 +10,77 @@ public class Turret : MonoBehaviour
     float distance;
 
     public GameObject projectile;
-
-    private Transform enemy;
-
-    Vector3 EnemyDistance;
+    private GameObject enemy;
 
     void Start()
     {
-        enemy = GameObject.FindGameObjectWithTag("Enemy").transform;
-
         timeBtwShots = startTimeBtwShots;
     }
 
     void Update()
     {
+		FindClosestEnemy();
+
 		//Finds the Enemy and start to instantiate projectiles
-        if (GameObject.FindWithTag("Enemy") != null)
+		if (enemy != null)
+		{
+			distance = Vector3.Distance(transform.position, enemy.transform.position);
+
+			//Check the distance to the nearest enemy and start shooting at him
+			if (distance <= 4)
+			{
+				if (timeBtwShots <= 0)
+				{
+					Instantiate(projectile, transform.position, Quaternion.identity);
+					timeBtwShots = startTimeBtwShots;
+				}
+				else
+				{
+					timeBtwShots -= Time.deltaTime;
+				}
+			}
+
+			RotateTowards(enemy.transform.position);
+		}
+		else
+		{
+			transform.rotation = Quaternion.Euler(0, 0, 0);
+		}
+	}
+
+	/// <summary>
+	/// Find the closest enemy
+	/// </summary>
+	/// <returns></returns>
+	public float FindClosestEnemy()
+    {        
+		float distance = Mathf.Infinity;
+
+        Vector3 position = transform.position;
+
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach (GameObject go in gos)
         {
-            EnemyDistance = GameObject.FindWithTag("Enemy").transform.position;
-            distance = Vector3.Distance(transform.position, EnemyDistance);
+            Vector3 diff = go.transform.position - position;
 
-            //Check the distance to the nearest enemy and start shooting at him
-            if (distance <= 4)
-            {
-                if (timeBtwShots <= 0)
-                {
-                    Instantiate(projectile, transform.position, Quaternion.identity);
-                    timeBtwShots = startTimeBtwShots;
-                }
-                else
-                {
-                    timeBtwShots -= Time.deltaTime;
-                }
+            float curDistance = diff.sqrMagnitude;
 
-                RotateTowards(enemy.position);
-            }
-            else
+            if (curDistance < distance)
             {
-                transform.rotation = Quaternion.Euler(0, 0, 0);
+				enemy = go;
+                distance = curDistance;
             }
         }
+        return distance;
     }
 
-    /// <summary>
-    /// Select the enemys position to rotate the Turret to the enemy
-    /// </summary>
-    /// <param name="target"></param>
-    private void RotateTowards(Vector2 target)
+	/// <summary>
+	/// Select the enemys position to rotate the Turret to the enemy
+	/// </summary>
+	/// <param name="target"></param>
+	private void RotateTowards(Vector2 target)
     {
         Vector2 direction = target - (Vector2)transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
