6c762b7053ab171b418314c84c04491e023e0b31
TheDragonHill <46721121+TheDragonHill@users.noreply.github.com>
2019-12-04 20:21:23 +0100

    Better Building

---
+++ Assets/Scripts/Crafting/Workbench.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Workbench : MonoBehaviour
+{
+	public GameObject CraftMenu;
+
+	private void OnTriggerStay2D(Collider2D player)
+	{
+		if (player.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.F))
+		{
+			CraftMenu.SetActive(true);
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D player)
+	{
+		if (player.gameObject.tag == "Player")
+		{
+			CraftMenu.SetActive(false);
+		}
+	}
+}
